f90e833981fb72d0b49f81fb3b7406b0993ffc8b
magu <95685391+itsmagu@users.noreply.github.com>
2022-08-21 23:48:47 +0200

    init

--- lowsystem.nix
+++ lowsystem.nix
@@ -7,7 +7,5 @@
 
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
-    networking.hostName = "nixos";
-    networking.networkmanager.enable = true;
     
 }
--- net.nix
+++ net.nix
@@ -3,6 +3,7 @@
 {
     networking = {
         hostName = "ViSuDevice";
+        networkmanager.enable = true;
         firewall = {
             enable = true;
             allowPing = false;
