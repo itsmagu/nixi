45967a544c00233ceb61460433327030a3ba2c1d
magu <95685391+itsmagu@users.noreply.github.com>
2022-08-22 20:36:00 +0200

    init-new

--- configuration.nix
+++ configuration.nix
@@ -1,14 +1,6 @@
 { config, pkgs, ... }:
 
-{	imports =
-  [ 
-  	./hardware.nix
-  	./lowsystem.nix
-  	./system.nix
-  	./software.nix
-  	./net.nix
-  	./desktop.nix
-  ];
+{
   system.copySystemConfiguration = true;
   system.stateVersion = "22.05";
 }
