f7c700d9da30126575d0a0992792968ee5bf120a
magu <95685391+itsmagu@users.noreply.github.com>
2022-08-22 20:09:20 +0200

    init

--- lowsystem.nix
+++ lowsystem.nix
@@ -5,7 +5,7 @@
         enable = false;
     };
 
-    boot.loader.systemd-boot.enable = true;
+    #boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     
 }
