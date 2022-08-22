2cc9b5407158494f2c7b805462d8dc3379bb9d00
magu <95685391+itsmagu@users.noreply.github.com>
2022-08-22 20:03:51 +0200

    init

--- lowsystem.nix
+++ lowsystem.nix
@@ -7,6 +7,6 @@
 
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
-    boot.loader.efi.efiSysMountPoint = "/boot/efi";
+    boot.loader.efi.efiSysMountPoint = "/boot";
     
 }
