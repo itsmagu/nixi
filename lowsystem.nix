9d76d8f1b93fb381324274f4532a58df24e89d4a
magu <95685391+itsmagu@users.noreply.github.com>
2022-08-24 21:25:51 +0200

    remove userspace

--- lowsystem.nix
+++ lowsystem.nix
@@ -8,4 +8,24 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     
+    services.xserver = {
+        enable = true;
+        displayManager = {
+          lightdm.enable = true;
+          lightdm.greeter.enable = true; 
+          #lightdm.background = ""
+          lightdm.autoLogin.timeout = 0;
+          autoLogin.enable = false;
+          autoLogin.user = "magu";
+          defaultSession = "none+awesome";
+        };
+
+        windowManager.awesome = {
+          enable = true;
+          luaModules = with pkgs.luaPackages; [
+            luarocks
+            luadbi-mysql
+          ];
+        };
+    };
 }
